c1a30ee3996e9be7e97b5d5c4c579f0b67f655dc
Petr Koutny <me@petrkoutny.cz>
2017-12-31 14:13:16 +0100

    Conditional spec of detector instance

--- spec/index.js
+++ spec/index.js
@@ -2,9 +2,13 @@ const pkg = require("browser-installations");
 const api = require("browser-installations/dist/api");
 
 const factory = new pkg.DetectorFactory();
-factory.create("chrome").detect().then(val => {
-    console.log(val);
-});
+const detector = factory.create("chrome");
+
+if (detector) {
+    detector.detect().then(val => {
+        console.log(val);
+    });
+}
 
 const v = api.Version.parse("1.2.3.4");
 console.log(v);
