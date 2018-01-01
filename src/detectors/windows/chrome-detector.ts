7fa620540c2ab3cd2107ad1b702b42b9bf6ef8fd
Petr Koutny <me@petrkoutny.cz>
2018-01-01 18:37:04 +0100

    Added win chrome detector constructor

--- src/detectors/windows/chrome-detector.ts
+++ src/detectors/windows/chrome-detector.ts
@@ -4,15 +4,21 @@ import {WindowsDetectorBase} from "./detector-base";
 export const EXECUTABLE_NAME = "chrome.exe";
 
 export class WindowsChromeDetector extends WindowsDetectorBase {
+    constructor() {
+        super();
+    }
+
     protected getExecutablePaths(): string[] {
         const paths: string[] = [];
 
         if (this.programFilesX86Path) {
             paths.push(path.join(this.programFilesX86Path, "Google", "Chrome", "Application", EXECUTABLE_NAME));
+            paths.push(path.join(this.programFilesX86Path, "Google", "Application", EXECUTABLE_NAME));
         }
 
         if (this.programFilesX64Path) {
             paths.push(path.join(this.programFilesX64Path, "Google", "Chrome", "Application", EXECUTABLE_NAME));
+            paths.push(path.join(this.programFilesX64Path, "Google", "Application", EXECUTABLE_NAME));
         }
 
         return paths;
