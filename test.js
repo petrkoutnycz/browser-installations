53efc55ba5eb34f60307dac65f16426fb7694731
Petr Koutny <petr.koutny@solarwinds.com>
2018-01-02 13:34:56 +0100

    Removed unnecessary test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const execSync = require("child_process").execSync;
-const ress = execSync(
-    "powershell -Command (Get-Item 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe').VersionInfo.FileVersion",
-    (err, stdout, stderr) => {
-        console.log(`stdout: ${stdout}`);
-
-    });
-console.log(`output: ${ress}`);
