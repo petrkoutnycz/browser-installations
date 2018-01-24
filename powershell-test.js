65ecc7ff2293668d88d98118e5eac13c7b313ee8
Petr Koutny <petr.koutny@solarwinds.com>
2018-01-24 10:32:13 +0100

    Removed powershell help file

--- powershell-test.js
+++
@@ -1,5 +0,0 @@
-const call = require("./dist/helpers/powershell-file-call").powershellFileCall;
-
-call("./assets/windows-chrome-updates.ps1", { "enable": 1 }).then(output => {
-    console.log(output);
-});
