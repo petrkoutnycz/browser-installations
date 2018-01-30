2cf9769af532f3287a64af1c0b7b7e11945e8807
Petr Koutny <petr.koutny@solarwinds.com>
2018-01-30 15:33:33 +0100

    Changed installation of chrome to be dynamic

--- .travis.before-install.sh
+++ .travis.before-install.sh
@@ -2,4 +2,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 sudo apt-get --assume-yes --purge remove google-chrome-stable
-sudo apt-get --assume-yes install google-chrome-stable=64.0.3282.119-1
+CHROME_VERSION=$(apt-cache show google-chrome-stable | grep Version | head -1 | sed -E 's/^[^0-9]+([0-9\.-]+*).*$/\1/g')
+echo Chrome version to install: $CHROME_VERSION
+sudo apt-get --assume-yes install google-chrome-stable=$CHROME_VERSION
+echo $CHROME_VERSION > spec/temp/chrome-version.txt
--- spec/src/travis-linux-detectors.spec.ts
+++ spec/src/travis-linux-detectors.spec.ts
@@ -1,20 +1,35 @@
+import * as fs from "fs";
+import * as path from "path";
+
 import {detectorFactory, IBrowserMetadata} from "browser-installations";
+import {Version} from "browser-installations/dist/api";
 
 if (process.env["TRAVIS"]) {
     describe("linux detector > ", () => {
-        it("chrome", async (done: DoneFn) => {
-            const d = detectorFactory("chrome");
-            let result: IBrowserMetadata;
-            try {
-                result = await d.detect();
-            } catch (err) {
-                done.fail(err);
-            }
+        describe("chrome >", () => {
+            let version: Version;
 
-            expect(result.versionObj.major).toBe(64);
-            expect(result.versionObj.minor).toBe(0);
-            expect(result.versionObj.patch).toBe(3282);
-            done();
+            beforeEach(() => {
+                const versionText = fs.readFileSync(path.join(__dirname, "../temp/chrome-version.txt"), {
+                    encoding: "utf-8"
+                });
+                version = Version.findFirst(versionText);
+            });
+
+            it("detects currently installed instance", async (done: DoneFn) => {
+                const d = detectorFactory("chrome");
+                let result: IBrowserMetadata;
+                try {
+                    result = await d.detect();
+                } catch (err) {
+                    done.fail(err);
+                }
+    
+                expect(result.versionObj.major).toBe(version.major);
+                expect(result.versionObj.minor).toBe(version.minor);
+                expect(result.versionObj.patch).toBe(version.patch);
+                done();
+            });
         });
 
         it("firefox", async (done: DoneFn) => {
